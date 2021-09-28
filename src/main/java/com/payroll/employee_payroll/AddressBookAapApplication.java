ce5b2d7cf681d9a95d2ccd393d4d3703fdf2264f
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-28 19:20:52 +0530

    [Nikhil] - Added intial setup

---
+++ src/main/java/com/payroll/employee_payroll/AddressBookAapApplication.java
@@ -0,0 +1,13 @@
+package com.payroll.employee_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAapApplication.class, args);
+	}
+
+}
