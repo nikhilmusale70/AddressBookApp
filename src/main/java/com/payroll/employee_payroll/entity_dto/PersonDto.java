0d2ebde42e5a22e32e1ee6d966a67e72be37544f
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-28 19:33:22 +0530

    [Nikhil] - added model and dto

---
+++ src/main/java/com/payroll/employee_payroll/entity_dto/PersonDto.java
@@ -0,0 +1,28 @@
+package com.payroll.employee_payroll.entity_dto;
+
+public class PersonDto {
+	String fname;
+	String lname;
+	int age;
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
---
+++ src/main/java/com/payroll/employee_payroll/model/PersonModel.java
@@ -0,0 +1,33 @@
+package com.payroll.employee_payroll.model;
+
+public class PersonModel {
+	String fname;
+	String lname;
+	char gender;
+	int age;
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
